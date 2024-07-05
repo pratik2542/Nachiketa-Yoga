32d33a21bc78864220dba45721079c215b5fbf4d
Pratiksinh Makwana <pratikmak2542@gmail.com>
2024-07-05 17:05:11 -0400

    sent html load

--- api/formHandler.js
+++ api/formHandler.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
         // Send AJAX request
         $.ajax({
             type: 'POST',
-            url: '/api/sendEmail', // Your API endpoint
+            url: 'https://nachiketa-yoga.vercel.app/api/sendEmail', // Your API endpoint
             data: formData,
             success: function(response) {
                 if (response.message === 'Email sent successfully') {
                     // Redirect to send.html on success
-                    window.location.href = 'sent.html';
+                    window.location.href = 'https://nachiketa-yoga.vercel.app/sent.html';
                 } else {
                     // Display error message
                     $('#result').text('Failed to send email: ' + response.message);
