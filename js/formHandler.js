461814f13c53b82fac996cd048130a631d20ad49
Pratiksinh Makwana <pratikmak2542@gmail.com>
2024-07-05 19:08:50 -0400

    change js

--- js/formHandler.js
+++ js/formHandler.js
@@ -8,18 +8,23 @@ $(document).ready(function() {
         // Send AJAX request
         $.ajax({
             type: 'POST',
-            url: 'https://nachiketa-yoga.vercel.app/api/sendEmail', // Your API endpoint
+            url: '/api/sendEmail', // Your API endpoint
             data: formData,
             success: function(response) {
+                console.log('Response from server:', response); // Log response for debugging
+
                 if (response.message === 'Email sent successfully') {
+                    console.log('Email sent successfully. Redirecting to sent.html');
                     // Redirect to send.html on success
-                    window.location.href = 'https://nachiketa-yoga.vercel.app/sent.html';
+                    window.location.href = '/sent.html';
                 } else {
+                    console.log('Failed to send email:', response.message);
                     // Display error message
                     $('#result').text('Failed to send email: ' + response.message);
                 }
             },
             error: function(error) {
+                console.error('An error occurred:', error);
                 // Display error message
                 $('#result').text('An error occurred: ' + error.responseText);
             }
